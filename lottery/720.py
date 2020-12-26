53f50e257460a6722d983302152b2e93076b5606
NetLSS <38928282+NetLSS@users.noreply.github.com>
2020-12-26 22:29:46 +0900

    mod: logic

--- lottery/720.py
+++ lottery/720.py
@@ -1,22 +1,40 @@
 import random
+from time import sleep
 
 random_offset = 1
+
+group_num = [1,2,3,4,5] # 조
+lottery_num = [0,1,2,3,4,5,6,7,8,9]
+
+# 7
+random.shuffle(lottery_num)
+sleep(0.07)
+random.shuffle(lottery_num)
+sleep(0.07)
+random.shuffle(lottery_num)
+sleep(0.07)
+random.shuffle(lottery_num)
+sleep(0.07)
+random.shuffle(lottery_num)
+sleep(0.07)
+random.shuffle(lottery_num)
+sleep(0.07)
+random.shuffle(lottery_num)
+sleep(0.07)
 
 loop_num = int(input("뽑을 개수:"))
 
 for i in range(loop_num):
 
-    group = random.randrange(1, 5 + random_offset) # 조
+    sleep(0.07)
+    group = random.choice(group_num) # 조
 
     lottery = []
 
     while True:
-    
-        num = random.randrange(0, 9 + random_offset)
+        sleep(0.07)
+        num = random.choice(lottery_num)
         
-        if num in lottery:
-            continue
-
         lottery.append(num)
 
         if len(lottery) == 6:
