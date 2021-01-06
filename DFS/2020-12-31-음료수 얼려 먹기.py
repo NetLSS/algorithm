c2141403c25a359b0e9803ba71fa07d1844c4c23
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-01-06 21:49:23 +0900

    DFS: 음료수 얼려 먹기

---
+++ DFS/2020-12-31-음료수 얼려 먹기.py
@@ -0,0 +1,48 @@
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))  # cf. str은 iterable
+
+
+def dfs(x, y): # x = 행, y = 열
+    # 범위 검사
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    # 현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
+
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
+
+
+'''
+15 14
+00000111100000
+11111101111110
+11011101101110
+11011101100000
+11011111111111
+11011111111100
+11000000011111
+01111111111111
+00000000011111
+01111111111000
+00011111111000
+11111111110011
+11100011111111
+11100011111111
+'''
