4ea343cb0b5c3b98d308d267cd5e21650c54e82a
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-01-21 22:47:05 +0900

    Binary Search: 이진 탐색

---
+++ Binary search/2021-01-21-이진 탐색 (재귀).py
@@ -0,0 +1,28 @@
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2  # 몫만 구하기
+
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:  # array[mid] < target:
+        return binary_search(array, target, mid + 1, end)
+
+
+n, target = list(map(int, input().split()))
+
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n - 1)
+
+if result == None:
+    print("원소가 존재하지 않음")
+else:
+    print(result + 1)
+
+'''
+10 7
+1 3 5 7 9 11 13 15 17 19
+'''
