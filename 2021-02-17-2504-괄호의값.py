7f43eb2eb6da2c19017f0a99e9f6e24f280826a8
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-02-18 19:47:28 +0900

    2021-02-17

--- 2021-02-17-2504-괄호의값.py
+++ 2021-02-17-2504-괄호의값.py
@@ -1,22 +1,63 @@
 
-s = input().replace("()","2").replace("[]","3")
+s = input()#.replace("()","2+").replace("[]","3+")
 
+def func(s):
+    # (()[[]])([]) # 28
+    stack = []
 
-# (()[[]])([]) # 28
-stack = []
-for i in range(len(s)):
-    if s[i] == '(' or s[i] == '[':
-        stack.append(s[i])
-    elif s[i] == ')':
-        while True:
-            p = stack.pop()
+    for i in range(len(s)):
+        if s[i] == '(' or s[i] == '[':
+            stack.append(s[i])
+        elif s[i] == ')':
+            e_v = ""
             sum = 0
-            if p.isdigit():
-                sum += int(p)
-            if p == '(':
-                sum *= 2
-                stack.append(str(sum)+"+")
-
-    elif s[i] == ']':
-
+            while True:
+                try:
+                    p = stack.pop()
+                except:
+                    print(0)
+                    return
+                if p == '(':
+                    if len(e_v) == 0:
+                        sum = 2
+                    else:
+                        try:
+                            sum = int(eval(e_v[:-1]))
+                            sum *= 2
+                        except:
+                            print(0)
+                            return
+                    stack.append(str(sum)+"+")
+                    break
+                else:
+                    e_v += p
+                
+        elif s[i] == ']':
+            e_v = ""
+            sum = 0
+            while True:
+                try:
+                    p = stack.pop()
+                except:
+                    print(0)
+                    return
+                if p == '[':
+                    if len(e_v) == 0:
+                        sum = 3
+                    else:
+                        try:
+                            sum = int(eval(e_v[:-1]))
+                            sum *= 3
+                        except:
+                            print(0)
+                            return
+                    stack.append(str(sum)+"+")
+                    break
+                else:
+                    e_v += p
+    try:
+        print(eval(''.join(stack)[:-1]))
+    except:
+        print(0)
     
+func(s)
