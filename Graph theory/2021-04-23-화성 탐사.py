dc5dce4a4ab0b5e5ef3f6ad154d68a2e71fd017c
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-04-23 22:55:43 +0900

    최단 경로: 화성 탐사 (트#2=ok)

--- Graph theory/2021-04-23-화성 탐사.py
+++ Graph theory/2021-04-23-화성 탐사.py
@@ -4,39 +4,73 @@ import sys
 
 input = sys.stdin.readline
 flush = sys.stdout.flush
+
+import heapq
 INF = int(1e9)
 
-def dij(a,b):
-    if vis_graph[a][b]:
-        return
-    vis_graph[a][b] = True
-
-    if b+1<n: #and vis_graph[a][b+1]: #우
-        dij_graph[a][b+1] = min(dij_graph[a][b+1], v_graph[a][b]+v_graph[a][b+1])
-    if b-1>=0: #and vis_graph[a][b-1]: # 좌
-        dij_graph[a][b-1] = min(dij_graph[a][b-1], v_graph[a][b]+v_graph[a][b-1])
-    if a-1>=0: #and vis_graph[a-1][b]: # 상
-        dij_graph[a-1][b] = min(dij_graph[a-1][b], v_graph[a][b]+v_graph[a-1][b])
-    if a+1<n: #and vis_graph[a+1][b]: # 하
-        dij_graph[a+1][b] = min(dij_graph[a+1][b], v_graph[a][b]+v_graph[a+1][b])
-
-
-
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
 
 T = int(input())
 for _ in range(T):
     n = int(input())
     v_graph = [[0]*(n) for _ in range(n)]
-    dij_graph = [[INF]*(n) for _ in range(n)]
-    vis_graph = [[False]*(n) for _ in range(n)]
+    distance = [[INF]*(n) for _ in range(n)]
     for i in range(n):
         v_graph[i] = list(map(int, input().split()))
 
-    dij_graph[0][0] = v_graph[0][0]
-    vis_graph[0][0] = True
+    x, y = 0, 0
+    distance[x][y] = v_graph[x][y]
+    q = [(distance[x][y], x, y)]
+
+    while q:
+        # 우선 순위 큐를 사용하여 최단 거리가 가장 짧은 노드 부터 pop
+        dist, x, y = heapq.heappop(q)
+        
+        # 노드가 while로 부터 이미 처리된 경우
+        if distance[x][y] < dist:
+            continue
+
+        # 현재 노드에서 각 방향으로 도착하는 거리가 원래 최단 거리 보다 짧은 경우 갱신
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+                continue
+
+            cost = dist + v_graph[nx][ny]
+            if distance[nx][ny] > cost:
+                distance[nx][ny] = cost
+                heapq.heappush(q, (cost, nx, ny))
     
-    for i in range(n):
-        for j in range(n):
-            dij(i, j)
-    
-    print(dij_graph)
+    # 각 테스트 케이스에 대한 정답 출력
+    print(distance[n-1][n-1])
+
+"""
+[input]
+3
+3
+5 5 4
+3 9 1 
+3 2 7
+5
+3 7 2 0 1
+2 8 0 9 1
+1 2 1 8 1
+9 8 9 2 0
+3 6 5 1 5
+7
+9 0 5 1 1 5 3
+4 1 2 1 6 5 3
+0 7 6 1 6 8 5
+1 1 7 8 3 2 3
+9 4 0 7 6 4 1
+5 8 3 2 4 8 3
+7 4 8 4 8 3 4
+
+[output]
+20
+19
+36
+"""
