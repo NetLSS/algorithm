bff2abe40aad3fe502f282a5a54f4531034edb85
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-04-26 11:40:49 +0900

    최단 경로: 숨바꼭질 (트#2=ok) (BFS)

--- Graph theory/2021-04-26-숨바꼭질.py
+++ Graph theory/2021-04-26-숨바꼭질.py
@@ -1,6 +1,7 @@
 # Q.40
-
+# my code (BFS)
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 flush = sys.stdout.flush
@@ -13,49 +14,38 @@ dx = [+0, +0, -1, +1]
 dy = [-1, +1, +0, +0]
 
 n, m = map(int, input().split())
-v_graph = [[INF]*(n+1) for _ in range(n+1)] # 거리 비용
-distance = [[INF]*(n+1) for _ in range(n+1)] # 최단 거리
 
-for i in range(n+1):
-    v_graph[i][i] = 0
-    distance[i][i] = 0
+graph = [[] for _ in range(n+1)]
+visit = [False for _ in range(n+1)]
+distance = [0 for _ in range(n+1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    v_graph[a][b] = 1
+    graph[a].append(b)
+    graph[b].append(a)
 
-q = [(distance[1][1], 1, 1)]
+start = 1
+distance[start] = 0
+visit[start] = True
+q = deque([start])
 
 while q:
-    dis, a, b = heapq.heappop(q)
+    start = q.popleft()
 
-    if dis > distance[a][b] :
-        continue
+    for b in graph[start]:
+        if not visit[b]:
+            distance[b] += distance[start] + 1
+            visit[b] = True
+            q.append(b)
 
-    for i in range(1,n+1):
-        if v_graph[a][i] != INF:
-            cost = distance[a][i] + dis
+max_distance = max(distance)
+barn_num = distance.index(max_distance)
+count = distance.count(max_distance)
 
-            if cost < distance[a][i]:
-                distance[a][i] = cost
-                heapq.heappush(q, (cost, a, i))
-    # for i in range(4):
-    #     cx = a+dx[i]
-    #     cy = b+dy[i]
-
-    #     if cx < 0 or cy < 0 or cx >= n+1 or cy >= n+1:
-    #         continue
-
-    #     cost = distance[cx][cy] + dis
-
-    #     if cost < distance[cx][cy]:
-    #         distance[cx][cy] = cost
-    #         heapq.heappush(q, (cost, cx, cy))
-    
-
-print(distance)
+print(barn_num, max_distance, count)
 
 """
+[input]
 6 7
 3 6
 4 3
@@ -64,4 +54,6 @@ print(distance)
 1 2
 2 4
 5 2
+[output]
+4 2 3
 """
