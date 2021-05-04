aedd0cdde533936293ab1ce838c10d847183a680
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-04 15:45:18 +0900

    그리디: 1이될때까지 (미처리 부분 커버)

--- Greedy/2021-05-04-1이될때까지.py
+++ Greedy/2021-05-04-1이될때까지.py
@@ -4,9 +4,14 @@ count = 0
 
 while n != 1:
     if n % k == 0:
-        n /= k
+        n //= k
+        count+=1
+    elif n < k:
+        count += n-1
+        n = 1
     else:
-        n -= 1
-    count += 1
+        rest = n % k
+        n = n - rest
+        count += rest
 
 print(count)   
