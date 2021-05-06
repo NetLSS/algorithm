ea1164292d6e96890a922402a87a68fee2b39380
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-06 12:02:32 +0900

    Mod: 코드 개선

--- BFS_DFS/DFS_example.py
+++ BFS_DFS/DFS_example.py
@@ -1,20 +1,12 @@
 def dfs(graph, v, visited):
-    if visited[v]:
-        return
-    else:
-        # 방문 처리
-        visited[v] = True 
-        print(v, end=' ')
+    # 방문 처리
+    visited[v] = True 
+    print(v, end=' ')
 
-        # 현재 노드와 연결된 노드 리스트
-        vs = graph[v]
-        # 오름차순 정렬 (미방문 노드 2개이상 시 작은 노드 부터 방문하도록)
-        vs.sort()
-
-        # 연결된 노드중 미방문 노드 방문 처리 (재귀)
-        for i in vs:
-            if visited[i] == False:
-                dfs(graph, i, visited)
+    # 연결된 노드중 미방문 노드 방문 처리 (재귀)
+    for i in sorted(graph[v]): # 2개 이상 시 낮은 번호 부터 방문하도록 정렬
+        if visited[i] == False:
+            dfs(graph, i, visited)
 
 
 # 그래프 인접 리스트 표현 방식 (가중치는 모두 동일 하다는 가정)
