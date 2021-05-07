0b488a0150aea38d28c280a53e4c7ddff0658a00
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-07 19:59:38 +0900

    정렬: 삽입정렬 다른 코드 추가

--- Sort/2021-05-07-삽입정렬.py
+++ Sort/2021-05-07-삽입정렬.py
@@ -1,5 +1,6 @@
-array = [ 7, 5, 9, 0, 3, 1, 6, 2, 4, 8, -9, -5, -6, -7]
+# 1
 
+array = [ 7, 5, 9, 0, 3, 1, 6, 2, 4, 8, -9, -5, -6, -7]
 
 for i in range(1, len(array)):
     breaked = False
@@ -15,4 +16,17 @@ for i in range(1, len(array)):
             breaked = True
             break
         
+print(array)
+
+# 2
+
+array = [ 7, 5, 9, 0, 3, 1, 6, 2, 4, 8, -9, -5, -6, -7]
+
+for i in range(1, len(array)):
+    for j in range(i,0,-1):
+        if array[j] < array[j-1]:
+            array[j],array[j-1] = array[j-1],array[j]
+        else:
+            break
+
 print(array)
