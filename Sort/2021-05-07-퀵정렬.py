fa915903a7b673825e0abe52a111614bf24255dd
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-09 23:37:05 +0900

    Chore: 퀵정렬 복습

--- Sort/2021-05-07-퀵정렬.py
+++ Sort/2021-05-07-퀵정렬.py
@@ -1,26 +1,38 @@
-array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+array = [6,9,7,8]
+# [6,5,4,3]
+#[5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+
 
 def quick_sort(arr, start, end):
-    p = start
-    l = start + 1
-    r = end
 
+    # 원소가 1개인 경우 종료
     if start >= end:
         return
+    
+    p = start # 첫 번째 데이터를 피벗으로 정함 (호어 분할 방식)
+    left = start + 1 
+    right = end 
 
-    while l <= r:
-        while l <= end and arr[l] <= arr[p]:
-            l += 1
-        while r > start and arr[r] >= arr[p]:
-            r -= 1
+    while left <= right:
+        
+        # 피벗 보다 큰 데이터 없으면 End+1 인덱스 까지 감
+        while left <= end and arr[left] <= arr[p]:
+            left += 1
 
-        if l > r:
-            arr[p], arr[r] = arr[r], arr[p]
+        # 피벗 보다 작은 데이터 없으면 start 즉 pivot 인덱스 까지 감
+        while right > start and arr[right] >= arr[p]:
+            right -= 1
+
+
+        if left > right:
+            arr[p], arr[right] = arr[right], arr[p]
+
         else:
-            arr[r], arr[l] = arr[l], arr[r]
+            arr[right], arr[left] = arr[left], arr[right]
 
-    quick_sort(arr, start, r-1)
-    quick_sort(arr, r+1, end)
+    quick_sort(arr, start, right-1)
+    quick_sort(arr, right+1, end)
 
 quick_sort(array, 0, len(array)-1)
 
