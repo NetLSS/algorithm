bbd46c7cce5eb3012c2a5276b432f2028f18b91c
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-10 22:39:48 +0900

    정렬: 계수 정렬 (기본 코드 작성)

--- Sort/2021-05-10-계수정렬.py
+++ Sort/2021-05-10-계수정렬.py
@@ -2,5 +2,11 @@
 
 array = [7,5,9,0,3,1,6,2,9,1,4,8,0,5,2]
 
-count = [ 0 for _ in range(len(array))]
+count = [ 0 for _ in range(9 - 0 + 1)]
 
+for item in array:
+    count[item] += 1
+
+for i, num in enumerate(count):
+    print(f"{i}"*num, end='')
+print()
