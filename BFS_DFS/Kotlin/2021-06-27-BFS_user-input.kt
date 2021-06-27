de02aaa7d817fa2ef5802920f78d856c7edb259e
NetLSS <futureprogrammer@naver.com>
2021-06-27 15:19:03 +0900

    BFS: Kotlin: BFS 기본 코드 코틀린화 + user input;

--- BFS_DFS/Kotlin/2021-06-27-BFS_user-input.kt
+++ BFS_DFS/Kotlin/2021-06-27-BFS_user-input.kt
@@ -1,3 +1,7 @@
+package bfsuserinput
+
+import java.util.*
+import kotlin.collections.ArrayDeque
 
 fun bfs(graph: MutableList<MutableList<Int>>, v: Int, visited: MutableList<Boolean>) {
     val q = ArrayDeque<Int>()
@@ -21,23 +25,49 @@ fun bfs(graph: MutableList<MutableList<Int>>, v: Int, visited: MutableList<Boole
 }
 
 fun main() {
-    val graph = mutableListOf<MutableList<Int>>(
-        mutableListOf(),
-        mutableListOf(2, 3, 8),
-        mutableListOf(1, 7),
-        mutableListOf(1, 4, 5),
-        mutableListOf(3, 5),
-        mutableListOf(3, 4),
-        mutableListOf(7),
-        mutableListOf(2, 6, 8),
-        mutableListOf(1, 7)
-    )
+    val v: Int
+    val graph: MutableList<MutableList<Int>>
 
+    with(Scanner(System.`in`)) {
+        v = readLine()!!.toInt() // readLine 으로 해야 복붙이 먹힘;
+
+        // 비어있는 2중 리스트 생성
+        graph=(0..v).map{
+            mutableListOf<Int>()
+        }.toMutableList()
+
+        // 각 버텍스(정점)에 연결된 엣지(간선) 받기
+        for(i in 1..v){
+            readLine()?.split(" ")?.map {
+                it.toIntOrNull()?.let { element ->
+                    graph[i].add(element)
+                }
+            }
+        }
+    }
+
+    // 방문 기록 불 리스트 생성. (모두 방문안함=false 로)
     val visited = (1..9).map { false }.toMutableList() // 타입 보는 방법 shift+control
 
+    // 1번 정점부터 방문.
     bfs(graph, 1, visited)
-    // 1 2 3 8 7 4 5 6
 }
+
+/*
+[input]
+8
+2 3 8
+1 7
+1 4 5
+3 5
+3 4
+7
+2 6 8
+1 7
+[output]
+1 2 3 8 7 4 5 6
+ */
+
 
 /* python
 from collections import deque
