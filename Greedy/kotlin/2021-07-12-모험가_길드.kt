587a88f3b40f29d584b3466528dbe20dc623691b
NetLSS <futureprogrammer@naver.com>
2021-07-14 16:33:05 +0900

    그리디: kt: 모험가 길드

---
+++ Greedy/kotlin/2021-07-12-모험가_길드.kt
@@ -0,0 +1,39 @@
+package t210712_1
+
+// 여행을 떠날 수 있는 그룹 수의 최대 값 구하기
+
+class Solution {
+    fun solution(n : Int, fearLevelList: List<Int>) : Int{
+        val sortedFearLevelList =  fearLevelList.sortedDescending()
+
+        var current = 0
+        var groupCnt = 0
+        while(current < n){
+
+            val fearLevel = sortedFearLevelList[current]
+
+            if(current + fearLevel - 1 < n){
+                groupCnt++
+            }
+
+            current += fearLevel
+
+        }
+
+        return groupCnt
+    }
+}
+
+fun main() {
+    val n: Int = readLine()!!.trim().toInt()
+    val fearLevelList: List<Int> = readLine()!!.trim().split(" ").map { it.toInt() }
+
+    println(Solution().solution(n, fearLevelList))
+}
+
+/*
+5
+2 3 1 2 2
+2
+
+ */
