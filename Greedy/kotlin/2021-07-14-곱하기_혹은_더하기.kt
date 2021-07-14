6d34d1d6493cd4da4e7041e168b18d16a63ffd2e
NetLSS <futureprogrammer@naver.com>
2021-07-14 16:54:53 +0900

    그리디: kt: 곱하기 혹은 더하기

---
+++ Greedy/kotlin/2021-07-14-곱하기_혹은_더하기.kt
@@ -0,0 +1,41 @@
+package t210712_2
+
+class Solution {
+    fun solution(str: String): Int {
+        if(str.length == 1){
+            return str.toInt()
+        }
+        // 주의 char 를 toInt 하면 아스키 코드값을 리턴하므로 문자열로 변경뒤 toInt() 해주어야함
+        var total = greedyPlusMulti(str[0].toString().toInt(),str[1].toString().toInt())
+
+        for (i in 2 until str.length){
+            total = greedyPlusMulti(total,str[i].toString().toInt())
+        }
+
+        return total
+    }
+
+    fun greedyPlusMulti(a:Int,b:Int): Int{
+        return maxOf(a*b,a+b)
+    }
+}
+
+fun main() {
+    val s = readLine()!!.trim()
+
+    println(Solution().solution(s))
+}
+
+/*
+02984
+576
+
+Process finished with exit code 0
+
+
+567
+210
+
+Process finished with exit code 0
+
+ */
