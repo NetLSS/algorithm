84ad5a414e05af29debed2f74aa1c232d5bbc280
NetLSS <futureprogrammer@naver.com>
2021-07-14 15:29:58 +0900

    그래프 이론: .kt: 커리큘럼 (위상정렬) 문제 잘못된 알고리즘 수정

--- Graph theory/kotlin/src/2021-06-29-커리큘럼(위상정렬).kt
+++ Graph theory/kotlin/src/2021-06-29-커리큘럼(위상정렬).kt
@@ -7,20 +7,20 @@ fun main() {
     val n = readLine()!!.toInt()
 
     val cost = (0..n).map { 0 }.toMutableList()
-    val inDegree = (0..n).map { 0 }.toMutableList()
+    val inDegree = (0..n).map { 0 }.toMutableList() // 차수
     val graph = (0..n).map { mutableListOf<Int>() }.toMutableList()
     val result = (0..n).map { Int.MIN_VALUE }.toMutableList()
 
     for (i in 1..n) {
         readLine()!!.trim().split(" ").mapIndexed { index, s ->
             when {
-                index == 0 -> {
+                index == 0 -> { // i 과목의 강의 시간
                     cost[i] = s.toInt()
                 }
-                s.toInt() == -1 -> {
+                s.toInt() == -1 -> { // 입력의 끝을 나타냄
                     return@mapIndexed
                 }
-                else -> {
+                else -> { // i 과목의 선수 과목
                     graph[s.toInt()].add(i)
                     inDegree[i]++
                 }
@@ -33,7 +33,7 @@ fun main() {
 
     for (i in 1..n) {
         val d = inDegree[i]
-        if (d == 0) {
+        if (d == 0) { // 차수가 0 인 녀석들만 넣어줌
             q.add(i)
             result[i] = cost[i]
         }
@@ -43,10 +43,11 @@ fun main() {
         val now = q.pop()
 
         for (d in graph[now]) {
+            result[d] = maxOf(result[d], result[now] + cost[d])
+
             inDegree[d]--
             if (inDegree[d] == 0) {
                 q.add(d)
-                result[d] = maxOf(result[d], result[now] + cost[d])
             }
         }
     }
@@ -64,4 +65,14 @@ fun main() {
 4 1 -1
 4 3 1 -1
 3 3 -1
+ */
+
+/*
+3
+30 -1
+20 -1
+40 1 2 -1
+30
+20
+70
  */
