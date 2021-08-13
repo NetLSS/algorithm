54d4950d14563068cbad49458d69480e63c475a5
NetLSS <futureprogrammer@naver.com>
2021-08-13 23:06:42 +0900

    DFS: 단어변환(queue) (RE)

---
+++ BFS_DFS/Kotlin/2021-08-13-단어변환(queue).kt
@@ -0,0 +1,57 @@
+import java.util.*
+import kotlin.math.min
+
+class Solution210813T1045 {
+    fun solution(begin: String, target: String, words: Array<String>): Int {
+        var answer = 0
+
+        if (target !in words)
+            return answer
+
+        data class Word(val depth: Int, val word: String, val isVisited: List<Boolean>)
+
+        val q : Queue<Word> = LinkedList()
+
+        q.add(Word(0, begin, List(words.size){false}))
+
+        while (q.isNotEmpty()) {
+            val currentWord = requireNotNull(q.poll())
+
+            currentWord.run {
+                if (word == target) {
+                    answer = if (answer == 0) {
+                        depth
+                    } else {
+                        min(depth, answer)
+                    }
+                    return@run
+                }
+
+                isVisited.forEachIndexed { index, b ->
+                    if (!b && isDiffOne(word, words[index])) {
+                        q.add(Word(depth+1, words[index], isVisited.toMutableList().apply { set(index,true) }))
+                    }
+                }
+            }
+        }
+        return answer
+    }
+
+    private fun isDiffOne(strA: String, strB: String): Boolean {
+        var diffCount = 0
+        strA.forEachIndexed { index, c ->
+            if(c != strB[index])
+                diffCount++
+            if(diffCount > 1)
+                return false
+        }
+
+        return diffCount == 1
+    }
+}
+
+/*
+begin	target	words	return
+"hit"	"cog"	["hot", "dot", "dog", "lot", "log", "cog"]	4
+"hit"	"cog"	["hot", "dot", "dog", "lot", "log"]	0
+ */
