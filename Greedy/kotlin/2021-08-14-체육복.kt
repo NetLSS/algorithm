1d8f10628a103afab7d107799f12a52f5369fb83
NetLSS <futureprogrammer@naver.com>
2021-08-14 18:33:27 +0900

    Greedy: 체육복

---
+++ Greedy/kotlin/2021-08-14-체육복.kt
@@ -0,0 +1,31 @@
+class Solution210814T1823 {
+    fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
+        var answer = 0
+
+        val realLost = (lost.toSet() - reserve.toSet()).sorted()
+        val realReserve = (reserve.toSet() - lost.toSet()).sorted().toMutableList()
+
+        answer += n - realLost.size
+
+        realLost.forEach {
+            if(it - 1 in realReserve){
+                answer++
+                realReserve.remove(it - 1)
+                return@forEach
+            } else if (it + 1 in realReserve){
+                answer++
+                realReserve.remove(it + 1)
+            }
+        }
+
+        return answer
+    }
+}
+/*
+입출력 예
+n	lost	reserve	return
+5	[2, 4]	[1, 3, 5]	5
+5	[2, 4]	[3]	4
+3	[3]	[1]	2
+
+ */
