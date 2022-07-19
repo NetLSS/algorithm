065ad2120fadf56430598c35d1bbd5fe0f3cf556
NetLSS <futureprogrammer@naver.com>
2022-07-19 21:54:58 +0900

    Add: BuyLottery.kt

---
+++ lottery/kotlin/BuyLottery.kt
@@ -0,0 +1,102 @@
+import kotlin.random.Random
+
+class BuyLottery {
+    private val groups = ArrayDeque<Int>(5).apply { addAll(listOf(1, 2, 3, 4, 5)) }
+    private val numbers = ArrayDeque<Int>(10).apply { addAll(List(10) { it }) }
+
+    init {
+        Thread.sleep(77)
+        groups.shuffle(Random(System.currentTimeMillis()))
+        Thread.sleep(77)
+        numbers.shuffle(Random(System.currentTimeMillis()))
+        Thread.sleep(77)
+        numbers.shuffle(Random(System.currentTimeMillis()))
+        Thread.sleep(77)
+        numbers.shuffle(Random(System.currentTimeMillis()))
+        Thread.sleep(77)
+        numbers.shuffle(Random(System.currentTimeMillis()))
+        Thread.sleep(77)
+        numbers.shuffle(Random(System.currentTimeMillis()))
+        Thread.sleep(77)
+        numbers.shuffle(Random(System.currentTimeMillis()))
+    }
+
+    fun buy720(buyCount: Int = 0) {
+        println("[720 연금 복권 구매]")
+        val pickCnt: Int = if (buyCount <= 0) {
+            print("뽑을 개수 : ")
+            readLine()!!.toInt()
+        } else {
+            buyCount
+        }
+
+        for (pCnt in 1..pickCnt) {
+            val winningGroup = groups.random(Random(System.currentTimeMillis()))
+            val winningNumber = mutableListOf<Int>()
+            for (i in 1..6) {
+                Thread.sleep(77)
+                winningNumber.add(numbers.random())
+            }
+
+            println("${winningGroup}조 $winningNumber")
+        }
+
+
+    }
+
+    fun buy6_45(buyCount: Int = 0) {
+        println("[6-45 로또 복권 구매]")
+        val numberBox = ArrayDeque<Int>()
+        val pickCount: Int = if (buyCount <= 0) {
+            print("뽑을 개수 : ")
+            readLine()!!.toInt()
+        } else {
+            buyCount
+        }
+
+        numberBox.run {
+            for (pick in 1..pickCount) {
+                val winningPicks = mutableListOf<Int>()
+                addAll(List<Int>(45) { it + 1 })
+
+                for (i in 1..6) {
+                    Thread.sleep(77)
+                    for (shuffle in 1..7) {
+                        shuffle(Random(System.currentTimeMillis()))
+                    }
+                    winningPicks.add(removeFirst())
+                }
+                println(winningPicks.sorted())
+                clear()
+            }
+        }
+    }
+
+    fun startBuyMachine() {
+        print("""
+            구매할 복권 종류
+            0 = 6-45
+            1 = 720
+            2 = 6-45, 720 둘다
+            입력 해주세요 :
+        """.trimIndent())
+        val buyType = readLine()!!.toInt()
+        when (buyType) {
+            0 -> {
+                buy6_45()
+            }
+            1 -> {
+                buy720()
+            }
+            2 -> {
+                buy720()
+                buy6_45()
+            }
+        }
+    }
+
+}
+
+fun main() {
+    BuyLottery().startBuyMachine()
+}
